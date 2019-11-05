002d043bde49a4d5731e5436442a360a3d7414c0
Jennie Zhang <jenniezhang@scsmac160.cs.uwaterloo.ca>
2019-11-05 15:49:21 -0500

    add story begin version 1 img

--- data/cards/cardList.js
+++ data/cards/cardList.js
@@ -1,10 +1,10 @@
 module.exports = [
   {
-    "id": "110011",
+    "id": "100111",
     "country": "Korea",
     "name": "The Story Begins Child Version 1 Nayeon",
     "date": "2015-10-20",
-    "series": "11001"
+    "series": "10011"
     "type": [
       "Nayeon",
       "Korea",
@@ -12,17 +12,390 @@ module.exports = [
       "The Story Begins"
     ]
   },
-    {
-    "id": "110012",
+  {
+    "id": "100112",
     "country": "Korea",
     "name": "The Story Begins Child Version 1 Jeongyeon",
     "date": "2015-10-20",
-    "series": "11001"
+    "series": "10011"
     "type": [
       "Jeongyeon",
       "Korea",
       "Album",
       "The Story Begins"
     ]
-  }
+  },
+  {
+    "id": "100113",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 1 Momo",
+    "date": "2015-10-20",
+    "series": "10011"
+    "type": [
+      "Momo",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100114",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 1 Sana",
+    "date": "2015-10-20",
+    "series": "10011"
+    "type": [
+      "Sana",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100115",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 1 Jihyo",
+    "date": "2015-10-20",
+    "series": "10011"
+    "type": [
+      "Jihyo",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100116",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 1 Mina",
+    "date": "2015-10-20",
+    "series": "10011"
+    "type": [
+      "Mina",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100117",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 1 Dahyun",
+    "date": "2015-10-20",
+    "series": "10011"
+    "type": [
+      "Dahyun",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100118",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 1 Chaeyeong",
+    "date": "2015-10-20",
+    "series": "10011"
+    "type": [
+      "Chaeyeong",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100119",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 1 Tzuyu",
+    "date": "2015-10-20",
+    "series": "10011"
+    "type": [
+      "Tzuyu",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+
+    {
+    "id": "100121",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 2 Nayeon",
+    "date": "2015-10-20",
+    "series": "10012"
+    "type": [
+      "Nayeon",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100122",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 2 Jeongyeon",
+    "date": "2015-10-20",
+    "series": "10012"
+    "type": [
+      "Jeongyeon",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100123",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 2 Momo",
+    "date": "2015-10-20",
+    "series": "10012"
+    "type": [
+      "Momo",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100124",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 2 Sana",
+    "date": "2015-10-20",
+    "series": "10012"
+    "type": [
+      "Sana",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100125",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 2 Jihyo",
+    "date": "2015-10-20",
+    "series": "10012"
+    "type": [
+      "Jihyo",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100126",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 2 Mina",
+    "date": "2015-10-20",
+    "series": "10012"
+    "type": [
+      "Mina",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100127",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 2 Dahyun",
+    "date": "2015-10-20",
+    "series": "10012"
+    "type": [
+      "Dahyun",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100128",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 2 Chaeyeong",
+    "date": "2015-10-20",
+    "series": "10012"
+    "type": [
+      "Chaeyeong",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100129",
+    "country": "Korea",
+    "name": "The Story Begins Child Version 2 Tzuyu",
+    "date": "2015-10-20",
+    "series": "10012"
+    "type": [
+      "Tzuyu",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100131",
+    "country": "Korea",
+    "name": "The Story Begins Adult Version Nayeon",
+    "date": "2015-10-20",
+    "series": "10013"
+    "type": [
+      "Nayeon",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100132",
+    "country": "Korea",
+    "name": "The Story Begins Adult Version Jeongyeon",
+    "date": "2015-10-20",
+    "series": "10013"
+    "type": [
+      "Jeongyeon",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100133",
+    "country": "Korea",
+    "name": "The Story Begins Adult Version Momo",
+    "date": "2015-10-20",
+    "series": "10013"
+    "type": [
+      "Momo",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100134",
+    "country": "Korea",
+    "name": "The Story Begins Adult Version Sana",
+    "date": "2015-10-20",
+    "series": "10013"
+    "type": [
+      "Sana",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100135",
+    "country": "Korea",
+    "name": "The Story Begins Adult Version Jihyo",
+    "date": "2015-10-20",
+    "series": "10013"
+    "type": [
+      "Jihyo",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100136",
+    "country": "Korea",
+    "name": "The Story Begins Adult Version Mina",
+    "date": "2015-10-20",
+    "series": "10013"
+    "type": [
+      "Mina",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100137",
+    "country": "Korea",
+    "name": "The Story Begins Adult Version Dahyun",
+    "date": "2015-10-20",
+    "series": "10013"
+    "type": [
+      "Dahyun",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100138",
+    "country": "Korea",
+    "name": "The Story Begins Adult Version Chaeyeong",
+    "date": "2015-10-20",
+    "series": "10013"
+    "type": [
+      "Chaeyeong",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100139",
+    "country": "Korea",
+    "name": "The Story Begins Adult Version Tzuyu",
+    "date": "2015-10-20",
+    "series": "10013"
+    "type": [
+      "Tzuyu",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100141",
+    "country": "Korea",
+    "name": "The Story Begins Unit Version MoJiCh",
+    "date": "2015-10-20",
+    "series": "10014"
+    "type": [
+      "Momo",
+      "Chaeyeong",
+      "Jihyo",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100142",
+    "country": "Korea",
+    "name": "The Story Begins Unit Version NaSaTz",
+    "date": "2015-10-20",
+    "series": "10014"
+    "type": [
+      "Nayeon",
+      "Sana",
+      "Tzuyu",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+  {
+    "id": "100143",
+    "country": "Korea",
+    "name": "The Story Begins Unit Version JeMiDa",
+    "date": "2015-10-20",
+    "series": "10014"
+    "type": [
+      "Jeongyeon",
+      "Mina",
+      "Dahyun",
+      "Korea",
+      "Album",
+      "The Story Begins"
+    ]
+  },
+
+
 ]
